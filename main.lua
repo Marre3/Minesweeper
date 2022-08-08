1d5b6e1ef843161e607bebedc59e88165470af55
Marre3 <marre244@gmail.com>
2022-08-08 13:53:37 +0200

    Set up basic field and drawing

--- main.lua
+++ main.lua
@@ -1,7 +1,21 @@
 function love.load()
-
+    field = {
+        width = 20,
+        height = 20
+    }
+    for i = 1, field.width do
+        field[i] = {}
+        for j = 1, field.height do
+            field[i][j] = 0
+        end
+    end
+    love.graphics.setNewFont(15)
 end
 
 function love.draw()
-
+    for i = 1, field.width do
+        for j = 1, field.height do
+            love.graphics.print(field[i][j], i*20, j*20)
+        end
+    end
 end
